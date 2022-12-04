9e8c6cbcf5d79726baef9e9ab8d5e8cebfec9932
Jalil David Salamé Messina <jalil.salame@tum.de>
2022-12-04 17:44:59 +0100

    feat: Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# Advent of Code Template
+
+This is a rust template that you can use with `cargo-generate` to quickly create
+a Advent of Code solution in Rust
---
+++ src/main.rs
@@ -0,0 +1,12 @@
+use color_eyre::Result;
+
+static INPUT: &str = include_str!("../input");
+
+fn main() -> Result<()> {
+    let p1_solution = "Nothing Yet";
+    let p2_solution = "Nothing Yet";
+
+    println!("Problem 1: {p1_solution}\nProblem 2: {p2_solution}");
+
+    Ok(())
+}
