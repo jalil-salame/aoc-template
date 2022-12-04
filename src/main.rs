4c4ee91669c00bd2b672ef999103ad12e8e9f867
Jalil David Salamé Messina <jalil.salame@tum.de>
2022-12-04 18:28:51 +0100

    feat: Improved template errors

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,16 @@
-use color_eyre::Result;
+use color_eyre::{Result, Report};
+
+#[allow(dead_code)]
+fn empty_option_err() -> Report {
+    use std::io::{Error, ErrorKind::Other};
+    Error::new(Other, "Option was empty").into()
+}
 
 static INPUT: &str = include_str!("../input");
 
 fn main() -> Result<()> {
+    color_eyre::install()?;
+
     let p1_solution = "Nothing Yet";
     let p2_solution = "Nothing Yet";
 
